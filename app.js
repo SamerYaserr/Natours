3ee42aa64d772734810082042907af71cdee72e9
Samer Samora <SamerSamora>
2025-03-06 21:21:04 +0200

    Simple Post API

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 const toursData = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -15,6 +16,26 @@ app.get('/api/v1/tours', (req, res) => {
       tours: toursData,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = toursData[toursData.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  toursData.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(toursData),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
