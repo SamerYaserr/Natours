ece84cf8cc78d22e1bd48eda8536eb792efd5f4d
Samer Samora <SamerSamora>
2025-03-11 21:46:50 +0200

    feat: complete tour modeling implementation
    - Finished the remaining functionality for tour modeling
    - Added missing features and refined the existing structure
    - Resolved any outstanding issues related to the implementation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -51,7 +51,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invaild data sent!',
+      message: err,
     });
   }
 };
@@ -77,9 +77,18 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
