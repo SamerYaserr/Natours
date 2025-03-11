09c71422b31364e8d50f9fbe9191c57df35a5173
Samer Samora <SamerSamora>
2025-03-11 17:27:56 +0200

    feat(controller): add createTour function to handle tour creation with mongoDB

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -33,13 +33,22 @@ exports.getTour = (req, res) => {
   });
 };
 
-exports.createTour = (req, res) => {
-  res.status(201).json({
-    status: 'success',
-    data: {
-      // tour: newTour,
-    },
-  });
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invaild data sent!',
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
