3096bbaf798fb08ca3ad06665123bf223945ea70
Samer Samora <SamerSamora>
2025-03-11 18:09:42 +0200

    feat(controller): add functionality to update documents in MongoDB

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,18 +1,9 @@
 const Tour = require('./../models/tourModel');
 
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
-
 exports.getAllTours = async (req, res) => {
   try {
     const tours = await Tour.find();
+
     res.status(200).json({
       status: 'Success',
       requestedAt: req.requestTime,
@@ -32,6 +23,7 @@ exports.getAllTours = async (req, res) => {
 exports.getTour = async (req, res) => {
   try {
     const tour = await Tour.findById(req.params.id);
+
     res.status(200).json({
       status: 'Success',
       data: {
@@ -64,13 +56,25 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: '<updated tour here...>',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.deleteTour = (req, res) => {
