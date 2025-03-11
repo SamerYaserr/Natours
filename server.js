986a1d70f77d55e8f1070d1df2c6e4aa22ac5478
Samer Samora <SamerSamora>
2025-03-11 16:14:58 +0200

    feat(model): add simple tour model with Mongoose

--- server.js
+++ server.js
@@ -13,6 +13,23 @@ mongoose.connect(DB).then(() => {
   console.log('DB connection successful!');
 });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: String,
+    require: [true, 'A tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
