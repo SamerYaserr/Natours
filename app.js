a9c9fcc581c839145d251ea57259b576c91eeeae
Samer Samora <SamerSamora>
2025-03-13 16:13:30 +0200

    feat: add global 404 route handler
    - Add pp.all('*') middleware to handle undefined routes
    - Return a 404 response with a JSON object containing status and message

--- app.js
+++ app.js
@@ -19,4 +19,11 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, err) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can’t find ${req.originalUrl} on this server!`,
+  });
+});
+
 module.exports = app;
