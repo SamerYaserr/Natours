57c3c2feb25318330c74b8663a9c5b1a936fba79
Samer Samora <samerzagloul@std.mans.edu.eg>
2025-05-03 01:55:09 +0300

    feat(auth): implement user login via API with form submission handling

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const cookieParser = require('cookie-parser');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -34,6 +35,8 @@ app.use('/api', limiter);
 
 app.use(express.json({ limit: '10kb' }));
 
+app.use(cookieParser());
+
 app.use(mongoSanitize());
 
 app.use(xss());
@@ -53,6 +56,7 @@ app.use(
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.cookies);
   next();
 });
 
