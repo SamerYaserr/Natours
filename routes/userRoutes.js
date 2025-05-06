a5b6217139fe0ba6522f59cd834a605180132e31
Samer Samora <samerzagloul@std.mans.edu.eg>
2025-05-06 20:36:43 +0300

    feat(users): add multer configuration for user photo uploads

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,7 +15,11 @@ router.use(authController.protect);
 
 router.get('/me', userController.getMe, userController.getUser);
 
-router.patch('/updateMe', userController.updateMe);
+router.patch(
+  '/updateMe',
+  userController.uploadUserPhoto,
+  userController.updateMe,
+);
 router.delete('/deleteMe', userController.deleteMe);
 
 router.patch('/updatePassword', authController.updatePassword);
