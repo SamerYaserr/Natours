d7d496b20916dbc308852e08aac4a546b1ef1715
Samer Samora <samerzagloul@std.mans.edu.eg>
2025-05-06 22:13:06 +0300

    feat(settings): enable image upload in user settings form

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,7 +21,7 @@ const upload = multer({
 exports.uploadUserPhoto = upload.single('photo');
 
 exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
-  if (!req.file) next();
+  if (!req.file) return next();
 
   req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
 
