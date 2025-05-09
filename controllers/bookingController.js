a3c9bf712ecf139ea95f3490b1e8b109dbe6f1b4
Samer Samora <samerzagloul@std.mans.edu.eg>
2025-05-09 23:55:39 +0300

    feat(payments): integrate Stripe checkout session for tour bookings

---
+++ controllers/bookingController.js
@@ -0,0 +1,36 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findById(req.params.tourId);
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    customer_email: req.user.email,
+    mode: 'payment',
+    success_url: `${req.protocol}://${req.get('host')}/`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    client_reference_id: req.params.tourId,
+    line_items: [
+      {
+        quantity: 1,
+        price_data: {
+          currency: 'usd',
+          unit_amount: tour.price * 100,
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+          },
+        },
+      },
+    ],
+  });
+
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
+});
