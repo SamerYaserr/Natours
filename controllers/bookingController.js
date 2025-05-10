af5fde3ce82dfb3a69c8154f427e54dff4852fe8
Samer Samora <samerzagloul@std.mans.edu.eg>
2025-05-10 15:17:53 +0300

    feat(bookings): implement full bookings API with route protection

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,5 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
+const factory = require('./handlerFactory');
 const Booking = require('../models/bookingModel');
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
@@ -44,3 +45,9 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
 
   res.redirect(req.originalUrl.split('?')[0]);
 });
+
+exports.getAllBookings = factory.getAll(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.createBooking = factory.createOne(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
